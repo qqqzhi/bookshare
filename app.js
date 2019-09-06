4a9f03b3a2cf9a4521affe423e19a8a59f5a509b
Yuan Zhi <qqqzhi@gmail.com>
2019-09-06 15:03:53 -0700

    Created routes for landing pages, show campgrounds, and create a new campground

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var express = require("express")
 var app = express();
+var bodyParser = require('body-parser');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({extended : true}));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var campgrounds = [
+		{name:"yellowstone", image:"https://media.istockphoto.com/photos/rainbow-in-a-thunderstorm-picture-id876109742"},
+		{name:"olympic", image:"https://media.istockphoto.com/photos/rainbow-in-a-thunderstorm-picture-id876109742"},
+		{name:"wawawai", image:"https://media.istockphoto.com/photos/rainbow-in-a-thunderstorm-picture-id876109742"}
+	];
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/", function(req, res) {
+	//res.send("server started")
+	res.render("landing.ejs")
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/campgrounds", function(req, res) {	
+	res.render("campgrounds.ejs", {campgrounds : campgrounds})
+})
+
+// npm install body-parse --save
+app.post("/campgrounds", function(req, res) { 
+	//res.send("you hit the url");
+	var name = req.body.name;
+	var imageurl = req.body.image;
+	var newCamp = {name : name, image : imageurl};
+	campgrounds.push(newCamp);
+	res.redirect("/campgrounds");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/campgrounds/new", function(req, res) {
+	res.render("new.ejs");
 });
 
-module.exports = app;
+app.listen(5000, function() {
+	console.log("server started")
+})
